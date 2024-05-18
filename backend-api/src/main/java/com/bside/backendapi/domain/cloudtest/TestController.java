fc60646d55e019d11b782fadb99f14a2598090a6
DongWon <ehddnjs1747@daum.net>
2024-05-18 09:20:07 +0900

    [feat] 클라우드 테스트용 기능

---
+++ backend-api/src/main/java/com/bside/backendapi/domain/cloudtest/TestController.java
@@ -0,0 +1,20 @@
+package com.bside.backendapi.domain.cloudtest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/test")
+public class TestController {
+
+    private final TestService testService;
+
+    @PostMapping("/test/api")
+    public String createTest(@RequestBody String text){
+        return testService.createTest(text);
+    }
+}
---
+++ backend-api/src/main/java/com/bside/backendapi/domain/cloudtest/TestService.java
@@ -0,0 +1,16 @@
+package com.bside.backendapi.domain.cloudtest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TestService {
+
+    private final TestRepository testRepository;
+
+    public String createTest(String text){
+        TestEntity testEntity = TestEntity.createTest(text);
+        return "저장성공";
+    }
+}
