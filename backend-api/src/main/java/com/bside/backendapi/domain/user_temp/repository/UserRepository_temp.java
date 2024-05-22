38cf37165dc063c49b378dbec388efbce510e208
DongWon <ehddnjs1747@daum.net>
2024-05-22 17:04:55 +0900

    [fix] 일단 User 삭제

--- backend-api/src/main/java/com/bside/backendapi/domain/user_temp/repository/UserRepository_temp.java
+++
@@ -1,7 +0,0 @@
-package com.bside.backendapi.domain.user_temp.repository;
-
-import com.bside.backendapi.domain.user_temp.entity.User_temp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository_temp extends JpaRepository<User_temp, Long> {
-}
