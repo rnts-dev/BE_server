bf720f45f4fb59ddbc9a9abf9d4344085a66ccf6
seyoungkwon <seyoungkwon29@gmail.com>
2024-06-24 20:04:43 +0900

    Create Appointment Test
    WithAuthUser.java 생성

--- backend-api/src/test/java/com/bside/backendapi/domain/appointment/domain/persist/AppointmentQueryRepositoryTest.java
+++ backend-api/src/test/java/com/bside/backendapi/domain/appointment/domain/persist/AppointmentQueryRepositoryTest.java
@@ -1,4 +1,16 @@
 package com.bside.backendapi.domain.appointment.domain.persist;
 
-public class AppointmentQueryRepositoryTest {
+import com.bside.backendapi.domain.member.domain.persist.Member;
+import com.bside.backendapi.domain.member.domain.persist.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AppointmentQueryRepositoryTest {
+
+    @Autowired AppointmentRepository appointmentRepository;
+    @Autowired MemberRepository memberRepository;
+    static Member member;
 }
