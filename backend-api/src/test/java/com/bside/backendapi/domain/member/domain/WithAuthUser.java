9546c4c6b5f29feb5297ecd9ad4d5f90142438d5
seyoungkwon <seyoungkwon29@gmail.com>
2024-07-26 18:07:43 +0900

    RestDocs, Test Code 주석처리

--- backend-api/src/test/java/com/bside/backendapi/domain/member/domain/WithAuthUser.java
+++ backend-api/src/test/java/com/bside/backendapi/domain/member/domain/WithAuthUser.java
@@ -1,17 +1,17 @@
-package com.bside.backendapi.domain.member.domain;
-
-import org.springframework.security.test.context.support.WithSecurityContext;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-@WithSecurityContext(factory = WithAuthUserSecurityContextFactory.class)
-public @interface WithAuthUser {
-    long id() default 1L;
-    String loginId() default "givenID";
-    String email() default "givenmail@naver.com";
-    String password() default "givenpw123!";
-    String name() default "테스트이름";
-    String nickname() default "givennick";
-}
+//package com.bside.backendapi.domain.member.domain;
+//
+//import org.springframework.security.test.context.support.WithSecurityContext;
+//
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//
+//@Retention(RetentionPolicy.RUNTIME)
+//@WithSecurityContext(factory = WithAuthUserSecurityContextFactory.class)
+//public @interface WithAuthUser {
+//    long id() default 1L;
+//    String loginId() default "givenID";
+//    String email() default "givenmail@naver.com";
+//    String password() default "givenpw123!";
+//    String name() default "테스트이름";
+//    String nickname() default "givennick";
+//}
